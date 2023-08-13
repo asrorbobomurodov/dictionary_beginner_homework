73f7471a2e4abbeaf9c52014bcba398bfe98b011
Asror Bobomurodov <asrorbobomurodov>
2023-08-13 14:09:15 +0500

    done by @asror

--- count_jobs.py
+++ count_jobs.py
@@ -8,4 +8,22 @@ def count_jobs(data:list, job:str) -> int:
     Returns:
         int: The number of users with the given job
     """
-    return 
+    count  = 0
+    for i in data:
+      if i['job'] == job:
+        count += 1
+    return count
+data=[
+  {
+    'name': 'John', 
+    'job': 'Developer'
+  }, 
+  {
+    'name': 'Mary', 
+    'job': 'Developer'
+  }
+  ]
+
+job = 'Developer'
+
+print(count_jobs(data, job))
